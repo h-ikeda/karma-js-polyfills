7ef609ec8f4cfbcd3db722d78e9821056f23d897
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2020-06-02 23:56:06 +0900

    test: ie cannot recognize arrow functions

--- test/integration_test.spec.js
+++ test/integration_test.spec.js
@@ -1,11 +1,11 @@
 describe('Karma test with polyfills', function suite() {
   it('Object.entries() should be able to be called', function test() {
-    expect(Object.entries({a: 1, b: 2})).to.deep.equal([['a', 1], ['b', 2]]);
+    expect(Object.entries({ a: 1, b: 2 })).to.deep.equal([['a', 1], ['b', 2]]);
   });
 });
 
 describe('Karma test without polyfills', function suite() {
   it('Object.entries() should not be able to be called', function test() {
-    expect(() => Object.entries({a: 1, b: 2})).to.throw(ReferenceError);
+    expect(function entries() { return Object.entries({ a: 1, b: 2 }); }).to.throw(ReferenceError);
   });
 });
